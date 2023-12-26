a05d0674d7b32ba022a72385494e8510c86eeed9
Moises Correa <correa.moises3@gmail.com>
2023-12-26 16:50:53 -0500

    move useConnect hook to a file and update deps

--- app/page.js
+++ app/page.js
@@ -1,107 +1,7 @@
 'use client'
-import React, { useEffect, useRef, useState } from "react"
-import Peer from "simple-peer"
-import io from "socket.io-client"
+import React, { useState } from "react"
 
-const useConnect = (url) => {
-  const socket = useRef(null)
-  const connectionRef = useRef(null)
-
-  const myVideo = useRef()
-  const externalVideo = useRef()
-
-  const [meetId, setMeetId] = useState(null)
-  const [meetStatus, setMeetStatus] = useState(null)
-  const [incomingCall, setIncomingCall] = useState(null)
-
-  useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
-      .then((stream) => {
-        myVideo.current.srcObject = stream
-      })
-
-    socket.current = io.connect(url)
-    socket.current.on("meetId", setMeetId)
-    socket.current.on("meetEnd", leaveMeet)
-    socket.current.on("joinMeet", ({ from, signal }) => {
-      setMeetStatus('requested')
-      setIncomingCall({ from, signal })
-    })
-
-    return leaveMeet;
-  }, [])
-
-  const joinMeet = (id) => {
-    const peer = new Peer({
-      initiator: true,
-      trickle: false,
-      stream: myVideo.current.srcObject
-    })
-
-    peer.on("signal", (signal) => {
-      socket.current.emit("joinMeet", {
-        from: meetId,
-        to: id,
-        signal,
-      })
-    })
-
-    peer.on("stream", (stream) => {
-      externalVideo.current.srcObject = stream
-    })
-
-    socket.current.on("acceptRequest", (signal) => {
-      setMeetStatus('online')
-      peer.signal(signal)
-    })
-
-    connectionRef.current = peer
-  }
-
-  const acceptRequest = () => {
-    if (incomingCall === null) return
-
-    setMeetStatus('online')
-
-    const peer = new Peer({
-      initiator: false,
-      trickle: false,
-      stream: myVideo.current.srcObject
-    })
-
-    peer.on("signal", (signal) => {
-      socket.current.emit("acceptRequest", { signal, to: incomingCall.from })
-    })
-
-    peer.on("stream", (stream) => {
-      externalVideo.current.srcObject = stream
-    })
-
-    peer.signal(incomingCall.signal)
-
-    connectionRef.current = peer
-  }
-
-  const leaveMeet = () => {
-    setMeetStatus(null)
-    if (connectionRef.current) {
-      connectionRef.current.destroy()
-    }
-  }
-
-  return {
-    id: meetId,
-    status: meetStatus,
-    myVideo,
-    externalVideo,
-    join: joinMeet,
-    accept: acceptRequest,
-    leave: leaveMeet,
-  }
-}
-
-function App() {
+export default function Page() {
   const [joinId, setJoinId] = useState('')
 
   const { id, status, myVideo, externalVideo, join, accept, leave } = useConnect('http://localhost:3001')
@@ -154,5 +54,3 @@ function App() {
     </>
   )
 }
-
-export default App
